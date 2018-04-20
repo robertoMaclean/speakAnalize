be5de8c6bc58580bb04f6da18c5ab00b3a86e8b4
robreznor <nemesis_nin@hotmail.com>
2018-04-19 23:50:30 -0300

    add amplitud to csv

--- lib/mic_array/transform.py
+++ lib/mic_array/transform.py
@@ -1,22 +1,32 @@
 import csv
+import wave
+import numpy as np
 
-def Transform(filenamein='output.txt', filenameout='output.csv'):
+def Transform(filenamein='output.txt', filenameout='output.csv', wavfile='output.wav'):
 	with open(filenamein,'r') as f:
 		content = f.readlines()
 	# you may also want to remove whitespace characters like `\n` at the end of each line
 	content = [x.strip() for x in content]
 	seconds = 0
+	wave_data, time = WaveData(wavfile)
+	print(len(wave_data))
 	with open(filenameout, 'w') as f:
-		fieldnames = ['direction','seconds','speak']
+		fieldnames = ['direction','seconds','speak','amplitude']
 		writer = csv.DictWriter(f, fieldnames=fieldnames, delimiter=";")
 		writer.writeheader()
 		mic = 0
-		for x in range(0,len(content)-1,2):		 
+		for x in range(0,len(content)-2,2):		 
 			for bit in content[x]:
 				if bit == '1':
 					mic = direction(int(content[x+1]))
 				seconds = seconds + 0.02
-				writer.writerow({'direction' : mic ,'seconds' : '{0:.2f}' .format(seconds),'speak': bit})
+				rang = np.where(((seconds-0.001) <= time) & (time <= (seconds+0.001)))
+				suma = 0
+				for y in rang[0]:
+					suma += abs(wave_data[y])
+		
+				prom = '{0:.2f}' .format(suma/len(rang[0]))
+				writer.writerow({'direction' : mic ,'seconds' : '{0:.2f}' .format(seconds),'speak': bit, 'amplitude': prom })
 
 def direction(degree):
 	if degree < 90:
@@ -27,6 +37,20 @@ def direction(degree):
 		return 2
 	elif degree < 360:
 		return 3
+
+def WaveData(wavfile):
+	f = wave.open(wavfile, 'r')
+	params = f.getparams()
+	nchannels, sampwidth, framerate, nframes = params[:4]
+	str_data = f.readframes(nframes)
+	f.close()
+	wave_data = np.fromstring(str_data, dtype=np.short)
+	wave_data.shape = -1, 4
+	wave_data = wave_data.T	
+	#Tiempo asociado a un frame, un frame es una posicion en el arreglo
+	time = np.arange(0, nframes) * (1.0 / framerate)
+	print(len(time))
+	return wave_data[0], time
 
 def main():
 	Transform()
