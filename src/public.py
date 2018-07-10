4f3c6fddae8eed75a053577313b709787d501778
robreznor <nemesis_nin@hotmail.com>
2018-07-09 21:34:57 -0400

    remove public.pyc

--- src/public.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def EnsureDir(filepath):
-	directory = os.path.dirname(filepath)
-	print filepath
-	print directory
-	if not os.path.exists(directory):
-		print "creando directorio", directory
-		os.makedirs(directory)
