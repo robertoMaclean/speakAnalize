11319b9590c10d0926ffd8f48e476511ffc2385c
robreznor <nemesis_nin@hotmail.com>
2018-07-11 02:58:16 -0400

    fix RATE

--- lib/mic_array/transform.py
+++ lib/mic_array/transform.py
@@ -3,7 +3,7 @@ import wave
 import numpy as np
 import time as tiempo
 
-def Transform(filenamein='../../files/data/output.txt', filenameout='../../files/data/output.csv', wavfile='../../files/data/output.wav'):
+def Transform(filenamein='../../files/data/output.txt', filenameout='../../files/data/output.csv', wavfile='../../files/data/output.wav', RATE = 16000):
 	with open(filenamein,'r') as f:
 		content = f.readlines()
 	# you may also want to remove whitespace characters like `\n` at the end of each line
@@ -22,12 +22,10 @@ def Transform(filenamein='../../files/data/output.txt', filenameout='../../files
 				if bit == '1':
 					mic = direction(int(content[x+1]))
 					#print(len(time)/16000)
-					seconds_left = int((seconds-0.0001)*16000)
-					seconds_right = int((seconds+0.0001)*16000) 
-					#print(seconds_left)
-					#print(seconds_right)
+					seconds_left = int((seconds-0.0001)*RATE)
+					seconds_right = int((seconds+0.0001)*RATE) 
 					short_time = time[seconds_left:seconds_right]
-					print(time[seconds_left:seconds_right])
+					#print(time[seconds_left:seconds_right])
 					#rang = np.where((seconds-0.0001 <= short_time) & (short_time <= seconds+0.0001))
 					#print('rang',rang[0])
 					#print('len',len(rang[0]))
@@ -40,7 +38,7 @@ def Transform(filenamein='../../files/data/output.txt', filenameout='../../files
 				 	div = 1
 					suma = 0	
 				prom = '{0:.2f}' .format(suma/div)
-				print('prom', prom)
+				#print('prom', prom)
 				writer.writerow({'direction' : mic ,'seconds' : '{0:.2f}' .format(seconds),'speak': bit, 'amplitude': prom })
 				pos += 1
 def direction(degree):
